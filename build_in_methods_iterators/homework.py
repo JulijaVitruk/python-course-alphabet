4f5bb8c885f7b7e09e9d98f76b556d7c399c21bf
Julija Vitruk <vl_ulia@ukr.net>
2019-04-24 01:56:00 +0300

    HW2-Python_functionality fix all problems

--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -18,18 +18,18 @@ def task_1_fix_names_start_letter(data: DT) -> DT:
         fix_names_start_letters([{'name': 'alex', 'age': 26}, {'name': 'denys', 'age': 89}])
         >>> [{'name': 'alex', 'age': 26}, {'name': 'denys', 'age': 89}]
     """
+
     def func(x: dict):
+        ret = None
         if x.get('name') is not None:
-            ret = x.get('name')
-        else: ret = '  '
+            ret = x.get('name')[0].upper()+x.get('name')[1:]
+            x['name']=ret
         return ret
 
-    our_list = list(map(lambda x: func(x), data))
-    our_list_up = list(map(lambda x: x[0].upper() + x[1:], our_list))
     for y in range(len(data)):
-        data[y]['name'] = our_list_up[y]
-    return data
+        func(data[y])
 
+    return data
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
@@ -40,12 +40,12 @@ def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
        remove_dict_field([{'name': 'alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
         >>> [{'name': 'alex'}, {'name': 'denys'}]
     """
-    data1=[]
+    data1 = []
     for a in range(len(data)):
         our_dict = data[a]
         for ke in redundant_keys:
             del our_dict[ke]
-        data1 += list(our_dict)
+        data1.append(our_dict)
 
     return data1
 
@@ -60,12 +60,18 @@ def task_3_find_item_via_value(data: DT, value) -> DT:
     return [x for x in data if value in x.values()]
 
 
-
 def task_4_min_value_integers(data: List[int]) -> int:
     """
     find and return minimum value from list
     """
-    return min(data)
+    if bool(data) is True:
+        our_min = min(data)
+    else:
+        our_min = None
+
+    return our_min
+
+
 
 
 def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
@@ -87,9 +93,9 @@ def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
     returns:
 
     """
-    list_of_val = list(map(lambda x: x[key], data))
 
-    return filter(lambda x: x[key] == min(list_of_val), data)
+    data1 = filter(lambda item: item.get(key) is not None, data)
+    return min(data1, key=lambda item: item.get(key))
 
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
@@ -134,21 +140,21 @@ def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
         next(a)
         >>> 3
     """
-    def gen_simp_val():
-        n = [2, 3]
-        yield 2
-        yield 3
-        i = 3
-        while i <= 210:
-            ost = [i % n[j] for j in range(len(n))]
-            if all(ost) is True:
-                yield i
-                n.append(i)
-                i += 1
-            else:
-                i += 1
-    gn = gen_simp_val()
-    print([x for x in gn])
+
+    n = [2, 3]
+    yield 2
+    yield 3
+    i = 3
+    while i <= 210:
+        ost = [i % n[j] for j in range(len(n))]
+        if all(ost) is True:
+            yield i
+            n.append(i)
+            i += 1
+        else:
+            i += 1
+
+
 
 
 
@@ -157,9 +163,9 @@ def task_11_create_list_of_random_characters() -> List[str]:
     create list of 20 elements where each element is random letter from latin alphabet
 
     """
-    list_of_rannum = [random.randint(0, len(string.ascii_letters)) for _ in range(20)]
+    list_of_rannum = [random.randint(0, len(string.ascii_lowercase)-1) for _ in range(20)]
 
-    [string.ascii_letters[list_of_rannum[x]] for x in range(20)]
+    return [string.ascii_lowercase[list_of_rannum[x]] for x in range(20)]
 
 
 
