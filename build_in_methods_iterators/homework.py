bf628eb14d3c758c284a6dfeed70b755248e8ede
JulijaVitruk <49599501+JulijaVitruk@users.noreply.github.com>
2019-05-03 15:37:40 +0300

    Develop (#1)
    * Change all task in homework
    
    * HW2-Python_functionality fix all problems
    
    * Change for PEP8
    
    * tasks1,2,4,7,11 fixed

--- build_in_methods_iterators/homework.py
+++ build_in_methods_iterators/homework.py
@@ -1,113 +1,139 @@
 from typing import List, Dict, Union, Generator
+import random
+import string
+import functools
 
-# We will work with such dicts
+
+# we will work with such dicts
 ST = Dict[str, Union[str, int]]
-# And we will put this dicts in list
+# and we will put this dicts in list
 DT = List[ST]
 
 
 def task_1_fix_names_start_letter(data: DT) -> DT:
     """
-    Make all `names` field in list of students to start from upper letter
+    make all `names` field in list of students to start from upper letter
 
-    Examples:
-        fix_names_start_letters([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}])
-        >>> [{'name': 'Alex', 'age': 26}, {'name': 'Denys', 'age': 89}]
+    examples:
+        fix_names_start_letters([{'name': 'alex', 'age': 26}, {'name': 'denys', 'age': 89}])
+        >>> [{'name': 'alex', 'age': 26}, {'name': 'denys', 'age': 89}]
     """
-    pass
+
+    return [{'name': x['name'].title(), 'age':x['age']} for x in data if x.get('name')]
 
 
 def task_2_remove_dict_fields(data: DT, redundant_keys: List[str]) -> DT:
     """given_data
-    Remove from dictionaries given key value
+    remove from dictionaries given key value
 
-    Examples:
-       remove_dict_field([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
-        >>> [{'name': 'Alex'}, {'name': 'denys'}]
+    examples:
+       remove_dict_field([{'name': 'alex', 'age': 26}, {'name': 'denys', 'age': 89}], 'age')
+        >>> [{'name': 'alex'}, {'name': 'denys'}]
     """
-    pass
+
+    return [{y: x[y] for y in x if y not in redundant_keys} for x in data]
 
 
 def task_3_find_item_via_value(data: DT, value) -> DT:
     """
-    Find and return all items that has @searching value in any key
-    Examples:
-        find_item_via_value([{'name': 'Alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
-        >>> [{'name': 'Alex', 'age': 26}]
+    find and return all items that has @searching value in any key
+    examples:
+        find_item_via_value([{'name': 'alex', 'age': 26}, {'name': 'denys', 'age': 89}], 26)
+        >>> [{'name': 'alex', 'age': 26}]
     """
-    pass
+    return [x for x in data if value in x.values()]
 
 
 def task_4_min_value_integers(data: List[int]) -> int:
     """
-    Find and return minimum value from list
+    find and return minimum value from list
     """
-    pass
+    return min(data, default = None)
 
 
 def task_5_min_value_strings(data: List[Union[str, int]]) -> str:
     """
-    Find the longest string
+    find the shortest string
     """
-    pass
+    result = None
+    dict_of_len = {str(x): len(str(x)) for x in data}
+    for x in dict_of_len:
+        if dict_of_len[x] == min(dict_of_len.values()):
+            result = x
+
+    return result
 
 
 def task_6_min_value_list_of_dicts(data: DT, key: str) -> ST:
     """
-    Find minimum value by given key
-    Returns:
-
+    find minimum value by given key()lambda
+    returns:
     """
-    pass
+    data1 = filter(lambda item: item.get(key) is not None, data)
+    return min(data1, key=lambda item: item.get(key))
 
 
 def task_7_max_value_list_of_lists(data: List[List[int]]) -> int:
     """
-    Find max value from list of lists
+    find max value from list of lists
     """
-    pass
+    return max(functools.reduce(lambda x, y: x+y, data))
 
 
 def task_8_sum_of_ints(data: List[int]) -> int:
     """
-    Find sum of all items in given list
+    find sum of all items in given list
     """
-    pass
+    return sum(data)
 
 
 def task_9_sum_characters_positions(text: str) -> int:
     """
-    Please read first about ascii table.
-    https://python-reference.readthedocs.io/en/latest/docs/str/ASCII.html
-    You need to calculate sum of decimal value of each symbol in text
-
-    Examples:
-        task_9_sum_characters_positions("A")
+    please read first about ascii table.
+    https://python-reference.readthedocs.io/en/latest/docs/str/ascii.html
+    you need to calculate sum of decimal value of each symbol in text
+        examples:
+        task_9_sum_characters_positions("a")
         >>> 65
         task_9_sum_characters_positions("hello")
         >>> 532
-
     """
-    pass
+
+    return sum([ord(text[i]) for i in range(len(text))])
 
 
 def task_10_generator_of_simple_numbers() -> Generator[int, None, None]:
     """
-    Return generator of simple numbers
-    Stop then iteration if returned value is more than 200
-    Examples:
+    return generator of simple numbers
+    stop then iteration if returned value is more than 200
+    examples:
         a = task_10_generator_of_simple_numbers()
         next(a)
         >>> 2
         next(a)
         >>> 3
     """
-    pass
+
+    n = [2, 3]
+    yield 2
+    yield 3
+    i = 3
+    while i <= 210:
+        ost = [i % n[j] for j in range(len(n))]
+        if all(ost):
+            yield i
+            n.append(i)
+            i += 1
+        else:
+            i += 1
 
 
 def task_11_create_list_of_random_characters() -> List[str]:
     """
-    Create list of 20 elements where each element is random letter from latin alphabet
+    create list of 20 elements where each element is random letter from latin alphabet
 
     """
-    pass
+    return random.choices(string.ascii_lowercase, k=20)
+
+
+
